83c0e8d03bc590880e83f6d27083f51584834df3
nestorrecinosUCA <00083120@uca.edu.sv>
2020-11-04 10:09:42 -0600

    :sparkles: migrations added

---
+++ database/migrations/2020_11_04_154525_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->string('lastname', 30);
+            $table->string('DUI', 10);
+            $table->string('NIT', 17);
+            $table->string('location', 100);
+            $table->integer('telephone');
+            $table->integer('cellphone');
+            $table->text('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
