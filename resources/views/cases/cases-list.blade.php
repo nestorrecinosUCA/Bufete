53db4b07c28748b5d6857954ad29f381187736e3
nestorrecinosUCA <00083120@uca.edu.sv>
2020-11-04 13:36:07 -0600

    Just some words changed

--- resources/views/cases/cases-list.blade.php
+++ resources/views/cases/cases-list.blade.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="icon" type="image/png" href="https://images.vexels.com/media/users/3/131163/isolated/lists/751175c268ea1f69854ff67fb93ee1a2-icono-de-circulo-de-martillo-de-juez.png">
-    <title>Bufete de abogados - Clientes</title>
+    <title>Bufete de abogados - Casos</title>
   </head>
 
   <body class="backg-color">
